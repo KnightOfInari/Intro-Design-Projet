b579f67c9d1c89ec76d1eff5ad4b6892c93389c5
KnightOfInari <maxime1.vincent@gmail.com>
2019-03-09 15:08:49 +0100

    Prototype
    prototype

---
+++ Intro-Design project/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * speed;
+        }
+    }
+}
